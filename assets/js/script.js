364eb649ef42c81e1a205e1666e9e32b17831b69
Dan Lyons <danielclyons@yahoo.com>
2020-04-12 12:28:50 -0700

    UX enhancements, Sec 5.5.8

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
-
+   console.log(taskEl) 
 };
 
 var createTask = function(taskText, taskDate, taskList) {
@@ -195,7 +195,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -234,15 +234,23 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag")
     console.log("activate", this);
   },
   deactivate: function(event) {
     console.log("deactivate", this);
+    $(".bottom-trash").removeClass("bottom-trash-drag")
+    $(this).removeClass("dropover")
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active")
+    $(".bottom-trash").addClass("bottom-trash-active")
     console.log("over", event.target);
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active")
+    $(".bottom-trash").removeClass("bottom-trash-active")
     console.log("out", event.target);
   },
   update: function(event) {
@@ -302,3 +310,9 @@ $("#modalDueDate").datepicker({
   minDate: 1
 });
 
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+ 
+  });
+}, (1000 * 60) * 30);
